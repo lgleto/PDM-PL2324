88bc8d339d94e3e7e4291c6c0e6a2b1f509d4f02
Lourenço <leto.lg@gmail.com>
2023-10-19 22:23:10 +0100

    Shopping list add and update

--- ShoppingList/app/src/main/java/ipca/utility/shoppinglist/MainActivity.kt
+++ ShoppingList/app/src/main/java/ipca/utility/shoppinglist/MainActivity.kt
@@ -1,5 +1,6 @@
 package ipca.utility.shoppinglist
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +11,7 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.ListView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,20 +21,43 @@ class MainActivity : AppCompatActivity() {
         Product("Pipocas",2 )
     )
 
+    val productAdapter = ProductAdapter()
+
+    val resultLauncher =
+        registerForActivityResult(ActivityResultContracts
+        .StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK){
+            it.data?.let {intent ->
+                val position = intent.extras?.getInt(ProductDetailActivity.DATA_POSITION)?:-1
+                val qtt = intent.extras?.getInt(ProductDetailActivity.DATA_QTT)
+                val name = intent.extras?.getString(ProductDetailActivity.DATA_PRODUCT_NAME)
+                if (position >= 0 ) {
+                    products[position].name = name!!
+                    products[position].qtt = qtt!!
+                }else {
+                    val product = Product(name!!, qtt!!)
+                    products.add(product)
+                }
+                productAdapter.notifyDataSetChanged()
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val listViewProducts = findViewById<ListView>(R.id.listViewProducts)
-        listViewProducts.adapter = ProductAdapter()
+        listViewProducts.adapter = productAdapter
 
         val buttonAdd = findViewById<Button>(R.id.buttonAddProduct)
         buttonAdd.setOnClickListener {
             val intent = Intent(this,ProductDetailActivity::class.java )
-            startActivity(intent)
+            resultLauncher.launch(intent)
         }
 
     }
+
+
 
     inner class ProductAdapter : BaseAdapter() {
 
@@ -57,6 +82,14 @@ class MainActivity : AppCompatActivity() {
             textViewProductName.text = products[position].name
             textViewProductQtt.text = products[position].qtt.toString()
             checkBox.isChecked = products[position].isChecked
+
+            rootView.setOnClickListener {
+                val intent = Intent(this@MainActivity,ProductDetailActivity::class.java )
+                intent.putExtra(ProductDetailActivity.DATA_PRODUCT_NAME, products[position].name)
+                intent.putExtra(ProductDetailActivity.DATA_QTT, products[position].qtt)
+                intent.putExtra(ProductDetailActivity.DATA_POSITION, position)
+                resultLauncher.launch(intent)
+            }
 
             return rootView
         }
--- ShoppingList/app/src/main/java/ipca/utility/shoppinglist/ProductDetailActivity.kt
+++ ShoppingList/app/src/main/java/ipca/utility/shoppinglist/ProductDetailActivity.kt
@@ -1,5 +1,7 @@
 package ipca.utility.shoppinglist
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ipca.utility.shoppinglist.databinding.ActivityProductDetailBinding
@@ -7,14 +9,53 @@ import ipca.utility.shoppinglist.databinding.ActivityProductDetailBinding
 class ProductDetailActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityProductDetailBinding
+    var position = -1
+    var qtd : Int
+        get() {
+            return binding.textViewQtt.text.toString().toInt()
+        }
+        set(value) {
+            if (value >= 0)
+                binding.textViewQtt.text = value.toString()
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.buttonIncrement.setOnClickListener {
-            binding.textViewQtt.text = 1.toString()
+
+        intent.extras?.let {
+            position = it.getInt(DATA_POSITION, -1)
+            qtd = it.getInt(DATA_QTT)
+            val name = it.getString(DATA_PRODUCT_NAME)
+            binding.editTextProductName.setText(name)
         }
 
+
+        binding.buttonIncrement.setOnClickListener {
+            qtd += 1
+        }
+        binding.buttonDecrement.setOnClickListener {
+            qtd -= 1
+        }
+
+        binding.buttonDone.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra(DATA_POSITION,position)
+            intent.putExtra(DATA_QTT,qtd)
+            intent.putExtra(
+                DATA_PRODUCT_NAME,
+                binding.editTextProductName.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+
+    }
+
+    companion object {
+        const val DATA_QTT = "data_qtt"
+        const val DATA_PRODUCT_NAME = "data_product_name"
+        const val DATA_POSITION = "data_position"
     }
 }

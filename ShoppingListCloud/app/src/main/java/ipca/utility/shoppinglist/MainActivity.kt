16ace9be9711f2fa0c39993f0b88356c32a2472c
Lourenço <leto.lg@gmail.com>
2023-12-07 18:56:44 +0000

    ShoppingListCloud push notifications

--- ShoppingListCloud/app/src/main/java/ipca/utility/shoppinglist/MainActivity.kt
+++ ShoppingListCloud/app/src/main/java/ipca/utility/shoppinglist/MainActivity.kt
@@ -2,6 +2,8 @@ package ipca.utility.shoppinglist
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,8 +15,14 @@ import android.widget.CheckBox
 import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import android.Manifest
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.messaging.FirebaseMessaging
 
 val TAG = "shoppinglist"
 
@@ -40,6 +48,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+
         db.collection("products")
             .addSnapshotListener { snapshots, e ->
                 if (e != null) {
@@ -62,7 +72,37 @@ class MainActivity : AppCompatActivity() {
                 }
                 productAdapter.notifyDataSetChanged()
             }
+        askNotificationPermission()
 
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = token
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            val auth = Firebase.auth
+            db.collection("users")
+                .document(auth.currentUser!!.uid)
+                .set(hashMapOf("fcm_token" to token ) )
+                .addOnSuccessListener { documentReference ->
+
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                    Toast.makeText(
+                        this,
+                        "Erro ao criar documento", Toast.LENGTH_LONG
+                    )
+                        .show()
+                }
+        })
     }
 
 
@@ -107,5 +147,35 @@ class MainActivity : AppCompatActivity() {
             return rootView
         }
 
+    }
+
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 }
